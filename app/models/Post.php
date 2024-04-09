a0e95d2a0d08280e155dab790cad7ced8655b91a
zmarsan <133293017+zmarsan@users.noreply.github.com>
2024-04-08 21:53:00 -0400

    Update Post.php

--- app/models/Post.php
+++ app/models/Post.php
@@ -4,5 +4,34 @@ namespace app\models;
 
 class Post
 {
-    //todo make methods here
+  // get posts
+    public function getAllPosts() {
+        $query = "select * from posts";
+        return $result->fetchAll();
+    }
+
+    // save post
+    public function saveNewPost($title, $description) {
+        $query = "insert into posts (post, description)";
+        return $this->queryWithParams($query, ['id' => $id]);
+    }    
+
+    // update
+    public function updatePost($title, $description) {
+        $query = "update posts set name = 'updated' where id = :id";
+        return $this->queryWithParams($query, ['id' => $id]);
+    }
+
+    // get by ID
+    public function getPostById($id) {
+        $query = ("select * from posts where id = :id");
+        return $this->queryWithParams($query,['id' => $id]);
+    }
+
+    // delete
+    public function deletePost($id) {
+       $query = ("delete from posts where id = :id");
+        return $this -> queryWithParams($query, ['id' => $id]);
+    }
+
 }
